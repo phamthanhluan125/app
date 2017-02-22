32d88cbe71d84a505516a1d9ab612b056f4f5d0e
Thanh Luan <bo.panda.qt@gmail.com>
2017-02-22 13:37:44 +0700

    Add account activation

--- APP/db/schema.rb
+++ APP/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222012348) do
+ActiveRecord::Schema.define(version: 20170222060901) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -22,11 +22,14 @@ ActiveRecord::Schema.define(version: 20170222012348) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
