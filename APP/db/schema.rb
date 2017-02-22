6f10969094389bb7a0980b5027810cbb5e284d05
Thanh Luan <bo.panda.qt@gmail.com>
2017-02-22 08:28:16 +0700

    Finish user edit, update, index, and destroy actions

--- APP/db/schema.rb
+++ APP/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221153702) do
+ActiveRecord::Schema.define(version: 20170222012348) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20170221153702) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
